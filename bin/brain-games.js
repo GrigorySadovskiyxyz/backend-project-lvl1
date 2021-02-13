c0485e4b65c61c80f285d265aa0e777a86d55b0f
Grigory Sadovskiy <sadovskiy.grigory@yandex.ru>
2021-02-13 15:10:15 +0300

    Added eslint, Makefile changed, devD changed

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-
-import { userNameInfo } from "../src/cli.js";
-
+// eslint-disable-next-line import/extensions
+import userNameInfo from '../src/cli.js';
+// eslint-disable-next-line no-unused-vars
--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,12 @@
-import { createRequire } from "module";
+/* eslint-disable no-console */
+import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
 
-const readlineSync = require("readline-sync");
+const readlineSync = require('readline-sync');
 
-console.log("Welcome to the Brain Games!");
-let userNameInfo = readlineSync.question("May I have your name? ");
-console.log("Hello, " + userNameInfo + "!");
+console.log('Welcome to the Brain Games!');
+const userNameInfo = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userNameInfo}!`);
 
-export { userNameInfo };
+export default userNameInfo;
