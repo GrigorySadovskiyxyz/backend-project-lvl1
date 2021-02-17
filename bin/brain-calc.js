b3b8fac92c375c2f0c33e644eff5d2c1f13b21d3
Grigory Sadovskiy <sadovskiy.grigory@yandex.ru>
2021-02-17 23:35:37 +0300

    brain-calc upd

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
+/* eslint-disable comma-dangle */
+import { createRequire } from 'module'; // Importing "require" for the readlineSync
+
+const require = createRequire(import.meta.url);
+const readlineSync = require('readline-sync'); // Importing readlineSync
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!\nWhat is the result of the expression?.`); // Assign name of the user
