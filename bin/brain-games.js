f999872f357aae82e5fa507b00b69e4e279047e2
Grigory Sadovskiy <sadovskiy.grigory@yandex.ru>
2021-06-17 23:16:05 +0300

    readline-sync installed

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
+import { userNameInfo } from '/Users/grigory/backend-project-lvl1/src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+
+const userName = userNameInfo()
+
+console.log(`Hello, ${userName}!`);
 
 console.log('Welcome to the Brain Games!');
---
+++ src/cli.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+import readlineSync from 'readline-sync';
+
+const userNameInfo = () => {
+	const userName = readlineSync.question('May I have your name? ');
+	return userName;
+};
+
+export { userNameInfo } ;
