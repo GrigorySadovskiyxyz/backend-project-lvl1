6769656829e0c7d5a1180434a9db519cc5a77fca
Grigory Sadovskiy <sadovskiy.grigory@yandex.ru>
2021-06-21 19:02:59 +0300

    brain-progression add

--- src/index.js
+++ src/index.js
@@ -11,4 +11,25 @@ const randomOperator = function getRandomOperator(value = 3) {
   return arr[Math.floor(Math.random() * value)];
 };
 
-export { randomValue, randomOperator };
+
+// Рекомендуемая длина прогрессии – 10 чисел.
+
+const randomArr = function getRandomArr() {
+  let arr = [];
+  let randomFirstValue = randomValue(100)
+  let randomSizing = randomValue(4)
+  let newVal = randomFirstValue + randomSizing;
+  arr.push(randomFirstValue);
+  
+  for (let i = 0; i < 9; i++) {
+    arr.push(newVal);
+    newVal += randomSizing;
+  }
+  return arr;
+}
+
+const randomIndex = function getRandomIndex(value = 9) {
+  return Math.floor(Math.random() * value);
+}; // Generating random number up to 100
+
+export { randomValue, randomOperator, randomArr, randomIndex };
