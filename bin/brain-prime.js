4e1900940bc267209e7ed55b647e15008f633f97
Grigory Sadovskiy <sadovskiy.grigory@yandex.ru>
2021-06-22 13:06:47 +0300

    brain-prime initial

---
+++ bin/brain-prime.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+/* eslint-disable comma-dangle */
+
+import readlineSync from 'readline-sync';
+import userNameInfoGreeting from '/Users/grigory/backend-project-lvl1/src/cli.js';
+import * as CONSTANTS from '/Users/grigory/backend-project-lvl1/src/index.js';
+
+const userName = userNameInfoGreeting();
+let counter = 0;
+
+console.log(
+    `Hello, ${userName}!\nAnswer "yes" if given number is prime. Otherwise answer "no".`
+  ); // Assign name of the user
+
+  function isPrime(num) {
+    if (num <= 1) return false;
+    if (num == 2) return true;
+  
+    for (let i = 2; i <= num / 2; i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function brainPrime() {
+    let trueAnswer;
+    do {
+    let givenNum = CONSTANTS.randomValue();
+      const initializeFunc = isPrime(givenNum);
+      console.log(`Question: ${givenNum}`);
+      const userAnswer = readlineSync.question('Your answer: ');
+      if ((userAnswer === 'yes') && (initializeFunc === true)) {
+        counter += 1;
+        console.log('Correct!');
+      } else if ((userAnswer === 'no') && (initializeFunc === false)) {
+        counter += 1;
+        console.log('Correct!'); 
+      } else {
+        trueAnswer = initializeFunc ? 'yes' : 'no';
+        console.log(
+          `'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`
+        );
+        break;
+      }
+    } while (counter !== 3);
+    if (counter === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+
+brainPrime()
