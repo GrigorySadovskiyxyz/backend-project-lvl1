c18bf88878415afb4bf323445f430164a5dc7807
Grigory Sadovskiy <sadovskiy.grigory@yandex.ru>
2021-06-22 13:10:53 +0300

    lint fix

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -10,45 +10,45 @@ const userName = userNameInfoGreeting();
 let counter = 0;
 
 console.log(
-    `Hello, ${userName}!\nAnswer "yes" if given number is prime. Otherwise answer "no".`
-  ); // Assign name of the user
+  `Hello, ${userName}!\nAnswer "yes" if given number is prime. Otherwise answer "no".`
+); // Assign name of the user
 
-  function isPrime(num) {
-    if (num <= 1) return false;
-    if (num == 2) return true;
-  
-    for (let i = 2; i <= num / 2; i++) {
-      if (num % i == 0) {
-        return false;
-      }
-    }
-    return true;
-  }
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num == 2) return true;
 
-  function brainPrime() {
-    let trueAnswer;
-    do {
-    let givenNum = CONSTANTS.randomValue();
-      const initializeFunc = isPrime(givenNum);
-      console.log(`Question: ${givenNum}`);
-      const userAnswer = readlineSync.question('Your answer: ');
-      if ((userAnswer === 'yes') && (initializeFunc === true)) {
-        counter += 1;
-        console.log('Correct!');
-      } else if ((userAnswer === 'no') && (initializeFunc === false)) {
-        counter += 1;
-        console.log('Correct!'); 
-      } else {
-        trueAnswer = initializeFunc ? 'yes' : 'no';
-        console.log(
-          `'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`
-        );
-        break;
-      }
-    } while (counter !== 3);
-    if (counter === 3) {
-      console.log(`Congratulations, ${userName}!`);
+  for (let i = 2; i <= num / 2; i++) {
+    if (num % i == 0) {
+      return false;
     }
   }
+  return true;
+}
 
-brainPrime()
+function brainPrime() {
+  let trueAnswer;
+  do {
+    const givenNum = CONSTANTS.randomValue();
+    const initializeFunc = isPrime(givenNum);
+    console.log(`Question: ${givenNum}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if ((userAnswer === 'yes') && (initializeFunc === true)) {
+      counter += 1;
+      console.log('Correct!');
+    } else if ((userAnswer === 'no') && (initializeFunc === false)) {
+      counter += 1;
+      console.log('Correct!');
+    } else {
+      trueAnswer = initializeFunc ? 'yes' : 'no';
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`
+      );
+      break;
+    }
+  } while (counter !== 3);
+  if (counter === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+}
+
+brainPrime();
